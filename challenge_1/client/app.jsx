90bd4c6e016eda7cf619f81937a7290659ee3bf9
ANAIT <Anait@ANAITs-MacBook-Pro.local>
2019-07-01 22:24:08 -0700

    Initiated app.jsx file, added bundle.js into gitignore file

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,6 +1,17 @@
-import React from ('react');
-import ReactDOM from ('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
 
-ReactDOM.render(<App />, document.getElementById('hef'));
+  render() {
+    return (
+      <h3>Historical Events Finder</h3>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('het'));
 
