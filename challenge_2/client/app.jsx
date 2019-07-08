b05d34eb108ee50fe06c5d7ef2bb7502de74d423
ANAIT <Anait@ANAITs-MacBook-Pro.local>
2019-07-07 18:28:37 -0700

    Initial set up, installed dependencies, created webpack.config file, basic server

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <h3>Cryptocurrency Chartig Tool</h3>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('cct'));
+
+
+
 
 
