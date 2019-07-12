51c3ae05971a7f8f3e2e70b10d8e64d69e33f1fb
ANAIT <Anait@ANAITs-MacBook-Pro.local>
2019-07-11 20:40:59 -0700

    Added get request to fetch data from coindesk

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      currency: [],
+      dates: []
+    }
   }
+  // date ?start=2019-06-01&end=2019-06-30
+  componentDidMount() {
+    axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
+      .then((response) => {
+        // return response;
+        console.log('RESPONSE', response.data);
+      })
+      .catch((error) => {
+        console.log('Error', error)
+      })
+  }
+
   render() {
     return (
       <h3>Cryptocurrency Chartig Tool</h3>
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+// const axios = require('axios');
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = 4000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/public')));
+
+// axios.get('https://api.coindesk.com/v1/bpi/historical/close.json') {
+//   .then((response) => {
+//   console.log('RESPONSE', response)
+// })
+//     .catch((error) => {
+//       console.log('Error', error)
+//     })
+// }
 
 app.listen(port, console.log(`Listening on port ${port}`));
 
